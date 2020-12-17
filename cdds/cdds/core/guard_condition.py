13039a0928e65f674e33fda71ff39cbbfa3a9353
Thijs Miedema <t.miedema@live.nl>
2020-12-17 11:49:18 +0100

    Added some docs

--- cdds/cdds/core/guard_condition.py
+++ cdds/cdds/core/guard_condition.py
@@ -5,15 +5,51 @@ from ctypes import POINTER, c_bool, byref
 
 
 class GuardCondition(Entity):
-    def __init__(self, domain_participant: 'cdds.domain.DomainParticipant'):
+    """ A GuardCondition is a manually triggered condition that can be added to a :class:`WaitSet<cdds.core.waitset.WaitSet>`."""
+
+    def __init__(self, domain_participant: 'DomainParticipant'):
+        """Initialize a GuardCondition
+        
+        Parameters
+        ----------
+        domain_participant: DomainParticipant
+            The domain in which the GuardCondition should be active.
+        """
+
         super().__init__(self._create_guardcondition(domain_participant._ref))
 
     def set(self, triggered: bool) -> None:
+        """Set the status of the GuardCondition to triggered or untriggered.
+
+        Parameters
+        ----------
+        triggered: bool
+            Wether to trigger this condition.
+
+        Returns
+        -------
+        None
+
+        Raises
+        ------
+        DDSException
+        """
         ret = self._set_guardcondition(self._ref, triggered)
         if ret < 0:
             raise DDSException(ret, f"Occurred when calling set on {repr(self)}")
 
     def read(self) -> bool:
+        """Read the status of the GuardCondition.
+
+        Returns
+        ----------
+        bool
+            Wether this condition is triggered.
+
+        Raises
+        ------
+        DDSException
+        """
         triggered = c_bool()
         ret = self._read_guardcondition(self._ref, byref(triggered))
         if ret < 0:
@@ -21,6 +57,17 @@ class GuardCondition(Entity):
         return bool(triggered)
 
     def take(self) -> bool:
+        """Take the status of the GuardCondition. If it is True it will be False on the next call.
+
+        Returns
+        ----------
+        bool
+            Wether this condition is triggered.
+
+        Raises
+        ------
+        DDSException
+        """
         triggered = c_bool()
         ret = self._take_guardcondition(self._ref, byref(triggered))
         if ret < 0:
